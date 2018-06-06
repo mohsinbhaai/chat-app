2a1ed08872faf3c75a3b3d76e4a4d193da061120
Mohsin Shaikh <mohsinshaikhms0@gmail.com>
2018-06-06 15:09:04 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+
+
+const container = require('./container');
+
+container.resolve(function(users) {
+
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/chatapp');
+
+    const app = SetupExpress();
+    
+    function SetupExpress() {
+        const app = express();
+        const server = http.createServer(app);
+        server.listen(3000, () => {
+            console.log('Listening port on 3000');
+        });
+
+        ConfigureExpress(app);
+
+        //Setup router
+        const router = require('express-promise-router')();
+        users.SetRouting(router);
+
+        app.use(router);
+
+    }
+
+    function ConfigureExpress(app) {
+        require('./passport/passport-local');
+
+        app.use(express.static('public'));
+        app.use(cookieParser());
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended:true}));
+
+        app.use(validator());
+        app.use(session({
+            secret: 'thisisascretkey',
+            resave: true,
+            saveUninitialized: true,
+            store: new MongoStore({mongooseConnection: mongoose.connection})
+        }));
+
+        app.use(flash());
+
+        app.use(passport.initialize());
+        app.use(passport.session());
+    }
+});
